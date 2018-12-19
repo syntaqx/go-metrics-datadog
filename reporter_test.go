fbfe71450d77193768b2aad4dd4d820efa10277a
Salil Apte <salil@ho-mbp-1073.local>
2018-12-19 13:25:47 -0800

    update unit test to cover fn option

--- reporter_test.go
+++ reporter_test.go
@@ -3,36 +3,34 @@ package datadog
 import "testing"
 
 func TestGetPercentileNamesOutOfRange(t *testing.T) {
-	_, err := getPercentileNames([]float64{0.23, 0})
-	if err == nil {
+	reporter := &Reporter{}
+	if UsePercentiles([]float64{0.23, 0})(reporter) == nil {
 		t.Fatal("Expected error")
 	}
-	_, err = getPercentileNames([]float64{0.23, 1})
-	if err == nil {
+	if UsePercentiles([]float64{0.23, 1})(reporter) == nil {
 		t.Fatal("Expected error")
 	}
-	_, err = getPercentileNames([]float64{0.23, -0.1})
-	if err == nil {
+	if UsePercentiles([]float64{0.23, -0.1})(reporter) == nil {
 		t.Fatal("Expected error")
 	}
-	_, err = getPercentileNames([]float64{0.23, 2})
-	if err == nil {
+	if UsePercentiles([]float64{0.23, 2})(reporter) == nil {
 		t.Fatal("Expected error")
 	}
 }
 func TestGetPercentileNames(t *testing.T) {
+	reporter := &Reporter{}
 	percentiles := []float64{0.23, 0.4, 0.99999, 0.45346356}
-	names, err := getPercentileNames(percentiles)
+	err := UsePercentiles(percentiles)(reporter)
 	if err != nil {
 		t.Fatal(err)
 	}
 	expectedNames := []string{".p23", ".p4", ".p99999", ".p45346356"}
-	if len(expectedNames) != len(names) {
-		t.Fatalf("Expected names: %v, got: %v", expectedNames, names)
+	if len(expectedNames) != len(reporter.p) {
+		t.Fatalf("Expected names: %v, got: %v", expectedNames, reporter.p)
 	}
 	for i, expectedName := range expectedNames {
-		if names[i] != expectedName {
-			t.Fatalf("Expected names: %v, got: %v", expectedNames, names)
+		if reporter.p[i] != expectedName {
+			t.Fatalf("Expected names: %v, got: %v", expectedNames, reporter.p)
 		}
 	}
 }
